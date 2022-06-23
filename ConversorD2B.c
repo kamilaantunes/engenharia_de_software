a2bd9f255132bd1c7a49ea8617ec8e2a165eee50
Kamila Antunes <kamilaantunes1@gmail.com>
2022-06-23 09:51:14 -0300

    Ajustando função desempilhar e leitura c/ argc

--- ConversorD2B.c
+++ ConversorD2B.c
@@ -110,8 +110,8 @@ int main(int argc, const char * argv[]){
 		return 0;
 	}
 
-	// Retorna para o início do arquivo
-	rewind(entrada);
+	rewind(entrada); // Retorna para o início do arquivo
+
 
 	while ((tst = fgetc(entrada)) != EOF){
 	if(!(tst =='0' || tst =='1' || tst =='2' || tst =='3' || tst =='4' || tst =='5' || tst =='6' || tst =='7' || tst =='8' || tst =='9' || tst =='\n')){
